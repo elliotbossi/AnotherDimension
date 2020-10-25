dcf75ea88bd17aa59d08f57ff85766969b94e59d
Elliot <ebossi10@stuy.edu>
2020-10-24 22:21:07 -0400

    third test

--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
     int[]arr = {0,2,1};
+    int[][]matrix = {{1,2},{1,3}};
     System.out.println(ArrayOps.sum(arr));
     System.out.println(ArrayOps.largest(arr));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(matrix)));
     }
 }
