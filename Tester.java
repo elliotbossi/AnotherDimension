685157552a0746a60a6e9f2450b0d9ceb7f1f76e
Elliot <ebossi10@stuy.edu>
2020-10-24 23:15:44 -0400

    switched test cases to those from google classroom

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
-    int[]arr = {0,2,1};
+    int[]arr = {1,3,5};
     int[][]matrix = {{1,  0, 12, -1 },{7, -2,  2,  1 },{ -5, -2,  2, -9 }};
     System.out.println(ArrayOps.sum(arr));
     System.out.println(ArrayOps.largest(arr));
