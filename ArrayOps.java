87e7f35af4c1106a519b551c8fd475c6a65d2914
Elliot <ebossi10@stuy.edu>
2020-10-24 22:07:20 -0400

    Second method worked

--- ArrayOps.java
+++ ArrayOps.java
@@ -3,10 +3,27 @@ public class ArrayOps {
   public static int sum(int[] arr) {
     int sum = 0;
     int i = 0;
+    if (arr.length == 0) {
+      return sum;
+    }
+    else {
+      while (i < arr.length) {
+        sum = sum + arr[i];
+        i = i + 1;
+      }
+      return sum;
+    }
+  }
+
+  public static int largest(int[] arr) {
+    int big = -100;
+    int i = 0;
     while (i < arr.length) {
-      sum = sum + arr[i];
+      if (arr[i] > big) {
+        big = arr[i];
+      }
       i = i + 1;
     }
-    return sum;
+    return big;
   }
 }
