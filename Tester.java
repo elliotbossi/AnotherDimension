6669c3c6243fc53873e93e0b23feb8f24dce812d
Elliot <ebossi10@stuy.edu>
2020-10-24 23:59:51 -0400

    updated tester

--- Tester.java
+++ Tester.java
@@ -3,10 +3,14 @@ public class Tester {
   public static void main(String[] args) {
     int[]arr = {1,3,5};
     int[][]matrix = {{1,  0, 12, -1 },{7, -2,  2,  1 },{ -5, -2,  2, -9 }};
+    int[][]x = {{4,2,2,2},{1,1,4,4},{2,4,1,1}};
     System.out.println(ArrayOps.sum(arr));
     System.out.println(ArrayOps.largest(arr));
     System.out.println(Arrays.toString(ArrayOps.sumRows(matrix)));
     System.out.println(Arrays.toString(ArrayOps.largestInRows(matrix)));
     System.out.println(ArrayOps.sum(matrix));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(matrix)));
+    System.out.println(ArrayOps.isRowMagic(x));
+    System.out.println(ArrayOps.isColMagic(x));
     }
 }
